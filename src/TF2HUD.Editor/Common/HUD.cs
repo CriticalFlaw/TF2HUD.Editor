ff848bc51353c8b5588ca0c114d10bfe7949ef6f
CriticalFlaw <nik_101@live.co.uk>
2021-01-26 23:28:39 -0500

    Updated json-gui to use default values
    - Added json files for flawhud and rayshud
    - Updated the HUD-GUI class to use default values.

--- src/TF2HUD.Editor/Common/HUD.cs
+++ src/TF2HUD.Editor/Common/HUD.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
+using System.Windows.Media;
+using Xceed.Wpf.Toolkit;
 
 namespace TF2HUD.Editor.Common
 {
@@ -13,6 +15,7 @@ namespace TF2HUD.Editor.Common
         public Dictionary<string, Control[]> ControlOptions;
         private bool ControlsRendered;
         public string CustomisationsFolder;
+        public string Default;
         public string EnabledFolder;
         public string Name;
         public string UpdateUrl;
@@ -37,24 +40,27 @@ namespace TF2HUD.Editor.Common
 
             // Title
             var CustomiserTitle = new Label();
-            CustomiserTitle.Content = this.Name;
+            CustomiserTitle.Content = Name;
             CustomiserTitle.FontSize = 35;
             CustomiserTitle.Margin = new Thickness(10, 10, 0, 0);
             Grid.SetRow(CustomiserTitle, 0);
             Container.Children.Add(CustomiserTitle);
- 
+
             var SectionsContainer = new WrapPanel();
+            SectionsContainer.Orientation = Orientation.Vertical;
+            SectionsContainer.MaxWidth = 1270;
+            SectionsContainer.MaxHeight = 720;
             Grid.SetColumn(SectionsContainer, 0);
             Grid.SetRow(SectionsContainer, 1);
 
-            var lastMargin = new Thickness(10, 10, 0, 0);
+            var lastMargin = new Thickness(10, 2, 0, 0);
             var lastTop = lastMargin.Top;
 
             foreach (var Section in ControlOptions.Keys)
             {
                 var SectionContainer = new GroupBox();
                 SectionContainer.Header = Section;
-                SectionContainer.Margin = new Thickness(10);
+                SectionContainer.Margin = new Thickness(5);
 
                 var SectionContent = new StackPanel();
                 SectionContent.Margin = new Thickness(5);
@@ -63,7 +69,8 @@ namespace TF2HUD.Editor.Common
                 {
                     var Label = ControlItem.Label;
                     var Type = ControlItem.Type;
-                    // var File = ControlItem.File;
+                    //var File = ControlItem.File;
+                    var Default = ControlItem.Default;
 
                     switch (Type)
                     {
@@ -74,10 +81,7 @@ namespace TF2HUD.Editor.Common
                             CharInputLabel.Content = Label;
                             var CharInput = new TextBox();
                             CharInput.Width = 60;
-                            CharInput.PreviewTextInput += (object sender, TextCompositionEventArgs e) =>
-                            {
-                                e.Handled = CharInput.Text != "";
-                            };
+                            CharInput.PreviewTextInput += (sender, e) => { e.Handled = CharInput.Text != ""; };
                             CharInputContainer.Children.Add(CharInputLabel);
                             CharInputContainer.Children.Add(CharInput);
                             SectionContent.Children.Add(CharInputContainer);
@@ -85,19 +89,30 @@ namespace TF2HUD.Editor.Common
                         case "Checkbox":
                             var cbCustomisation = new CheckBox();
                             cbCustomisation.Content = Label;
-                            cbCustomisation.Margin = new Thickness(10, lastTop, 0, 0);
+                            cbCustomisation.Margin = new Thickness(10, lastTop + 10, 0, 0);
                             lastMargin = cbCustomisation.Margin;
+                            cbCustomisation.IsChecked = ControlItem.Default == "1";
                             SectionContent.Children.Add(cbCustomisation);
                             break;
                         case "Color":
                         case "Colour":
+                        case "ColorPicker":
                             var ColourInputContainer = new StackPanel();
                             ColourInputContainer.Margin = new Thickness(10, lastTop, 0, 10);
                             var ColoutInputLabel = new Label();
                             ColoutInputLabel.Content = Label;
                             ColoutInputLabel.FontSize = 16;
-                            var ColourInput = new Xceed.Wpf.Toolkit.ColorPicker();
-                            ColourInput.SelectedColor = System.Windows.Media.Color.FromArgb(255, 0, 255, 0);
+                            var ColourInput = new ColorPicker();
+                            try
+                            {
+                                ColourInput.SelectedColor =
+                                    (Color) new ColorConverter().ConvertFrom(ControlItem.Default);
+                            }
+                            catch
+                            {
+                                ColourInput.SelectedColor = Color.FromArgb(255, 0, 255, 0);
+                            }
+
                             ColourInputContainer.Children.Add(ColoutInputLabel);
                             ColourInputContainer.Children.Add(ColourInput);
                             SectionContent.Children.Add(ColourInputContainer);
@@ -106,9 +121,11 @@ namespace TF2HUD.Editor.Common
                         case "DropDownMenu":
                         case "Select":
                         case "ComboBox":
-                            var ComboBoxContainer = new WrapPanel();
+                            var ComboBoxContainer = new StackPanel();
+                            ComboBoxContainer.Margin = new Thickness(10, lastTop, 0, 10);
                             var ComboBoxLabel = new Label();
                             ComboBoxLabel.Content = Label;
+                            ComboBoxLabel.FontSize = 16;
                             var ComboBoxCustomisation = new ComboBox();
                             if (ControlItem.Options == null) break;
                             if (ControlItem.Options.Length <= 0) break;
@@ -118,12 +135,13 @@ namespace TF2HUD.Editor.Common
                                 var OptionValue = option.Value;
                                 ComboBoxCustomisation.Items.Add(OptionLabel);
                             }
+
+                            ComboBoxCustomisation.SelectedIndex = int.TryParse(ControlItem.Default, out var index) ? index : 1;
                             ComboBoxContainer.Children.Add(ComboBoxLabel);
                             ComboBoxContainer.Children.Add(ComboBoxCustomisation);
 
                             ComboBoxContainer.Margin = new Thickness(10, lastTop, 0, 10);
                             SectionContent.Children.Add(ComboBoxContainer);
-
                             break;
                         case "Number":
                             var NumberInputContainer = new WrapPanel();
@@ -132,13 +150,28 @@ namespace TF2HUD.Editor.Common
                             NumberInputLabel.Content = Label;
                             var NumberInput = new TextBox();
                             NumberInput.Width = 60;
-                            NumberInput.PreviewTextInput += (object sender, TextCompositionEventArgs e) =>
+                            NumberInput.PreviewTextInput += (sender, e) =>
                             {
-                                e.Handled = !System.Text.RegularExpressions.Regex.IsMatch(e.Text, "\\d");
+                                e.Handled = !Regex.IsMatch(e.Text, "\\d");
                             };
                             NumberInputContainer.Children.Add(NumberInputLabel);
                             NumberInputContainer.Children.Add(NumberInput);
                             SectionContent.Children.Add(NumberInputContainer);
+                            break;
+                        case "IntegerUpDown":
+                            var IntegerInputContainer = new StackPanel();
+                            IntegerInputContainer.Margin = new Thickness(10, lastTop, 0, 10);
+                            var IntegerInputLabel = new Label();
+                            IntegerInputLabel.Content = Label;
+                            IntegerInputLabel.FontSize = 16;
+                            var IntegerInput = new IntegerUpDown();
+                            IntegerInput.Value = int.TryParse(ControlItem.Default, out index) ? index : 0;
+                            IntegerInput.Minimum = ControlItem.Minimum;
+                            IntegerInput.Maximum = ControlItem.Maximum;
+                            IntegerInput.Increment = ControlItem.Increment;
+                            IntegerInputContainer.Children.Add(IntegerInputLabel);
+                            IntegerInputContainer.Children.Add(IntegerInput);
+                            SectionContent.Children.Add(IntegerInputContainer);
                             break;
                         default:
                             throw new Exception($"Type {Type} is not a valid type!");
@@ -146,9 +179,11 @@ namespace TF2HUD.Editor.Common
 
                     // lastTop = lastMargin.Top + 10;
                 }
+
                 SectionContainer.Content = SectionContent;
                 SectionsContainer.Children.Add(SectionContainer);
             }
+
             Container.Children.Add(SectionsContainer);
             Controls.Children.Add(Container);
 
@@ -178,21 +213,26 @@ namespace TF2HUD.Editor.Common
 
     public class Control
     {
-        [JsonPropertyName("File")] public string Label;
+        [JsonPropertyName("Default")] public string Default = "0";
 
-        [JsonPropertyName("File")] public string Type;
+        [JsonPropertyName("Increment")] public int Increment = 2;
+        [JsonPropertyName(";")] public string Label;
 
-        [JsonPropertyName("File")] public Option[] Options;
+        [JsonPropertyName("Maximum")] public int Maximum = 30;
 
+        [JsonPropertyName("Minimum")] public int Minimum = 10;
+
+        [JsonPropertyName("Options")] public Option[] Options;
+
+        [JsonPropertyName("Type")] public string Type;
     }
 
-	public class Option
-	{
-        [JsonPropertyName("File")] public string Label;
+    public class Option
+    {
+        [JsonPropertyName("Label")] public string Label;
 
-        [JsonPropertyName("File")] public string Value;
+        [JsonPropertyName("Value")] public string Value;
     }
-
 
     #endregion
 }
