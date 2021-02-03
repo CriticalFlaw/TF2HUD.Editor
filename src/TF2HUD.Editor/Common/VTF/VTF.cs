4583e891be5d78bce760c2bc1aaf3862abd30cc1
CriticalFlaw <nik_101@live.co.uk>
2021-02-03 17:01:40 -0500

    Added Revan's VTF Converter
    Co-Authored-By: cooolbros <43595566+cooolbros@users.noreply.github.com>

---
+++ src/TF2HUD.Editor/Common/VTF/VTF.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+
+namespace TF2HUD.Editor.Common
+{
+    internal class VTF
+    {
+        private readonly string TF2Path;
+
+        public VTF(string TF2Path)
+        {
+            this.TF2Path = TF2Path.Replace("\\tf\\custom", string.Empty);
+        }
+
+        public void Convert(string InFile, string OutFile)
+        {
+            // Resize image to square of larger proportional
+            var Image = new Bitmap(InFile);
+            var SquareImage = ResizeImage(Image);
+
+            var MateralSrc = $"{TF2Path}\\tf\\materialsrc";
+
+            // Create materialsrc (ensure it exists)
+            Directory.CreateDirectory(MateralSrc);
+
+            // Save image as .tga (cast using TGASharpLib)
+            var TGASquareImage = (TGA) SquareImage;
+            TGASquareImage.Save($"{MateralSrc}\\temp.tga");
+
+            // Convert using VTEX
+            VTEXConvert(MateralSrc, "temp");
+
+            // Path to VTEX output file
+            var VTFLocation = $"{TF2Path}\\tf\\materials\\temp.vtf";
+
+            // Create absolute path to output folder and make directory
+            var PathInfo = OutFile.Split('\\', '/');
+            PathInfo[PathInfo.Length - 1] = "";
+            var FolderPath = string.Join("\\", PathInfo);
+            Directory.CreateDirectory(FolderPath);
+
+            // Copy vtf from vtex output to user defined path
+            File.Copy(VTFLocation, OutFile, true);
+
+            // Delete temporary tga and vtex output
+            File.Delete($"{MateralSrc}\\temp.tga");
+            File.Delete($"{MateralSrc}\\temp.txt");
+            File.Delete(VTFLocation);
+        }
+
+        private Bitmap ResizeImage(Bitmap Image)
+        {
+            var ImageMaxSize = Math.Max(Image.Width, Image.Height);
+            var Power = 2;
+            do
+            {
+                Power *= 2;
+            } while (Power < ImageMaxSize);
+
+            // Paint G onto SquareImage Bitmap
+            var SquareImage = new Bitmap(Power, Power);
+            var G = Graphics.FromImage(SquareImage);
+            G.DrawImage(Image, 0, 0, Power, Power);
+
+            return SquareImage;
+        }
+
+        private void VTEXConvert(string FolderPath, string FileNameNoExt)
+        {
+            // VTEX Args
+            // https://developer.valvesoftware.com/wiki/Vtex_compile_parameters
+            File.WriteAllLines($"{FolderPath}\\{FileNameNoExt}.txt", new[]
+            {
+                "pointsample 1",
+                "nolod 1",
+                "nomip 1"
+            });
+
+            // VTEX CLI Args
+            string[] Args =
+            {
+                $"\"{FolderPath}\\{FileNameNoExt}.tga\"",
+                "-nopause",
+                "-game",
+                $"\"{TF2Path}\\tf\\\""
+            };
+
+            var ArgsString = string.Join(" ", Args);
+            Process.Start($"{TF2Path}\\bin\\vtex.exe", ArgsString).WaitForExit();
+        }
+    }
+}
