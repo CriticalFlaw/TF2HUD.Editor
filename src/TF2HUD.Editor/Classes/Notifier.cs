79105e8a64cb5cd69d3f5d3f9fa253fc40c7b776
Adam Swanson <adam.swanson.98@gmail.com>
2021-08-29 18:49:08 -0400

    feat: add Notifier class for displaying a MessageBox and logging

---
+++ src/TF2HUD.Editor/Classes/Notifier.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HUDEditor.Classes
+{
+    public class Notifier
+    {
+        private readonly ILog _logger;
+
+        public Notifier(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///     Display a message box with a message to the user and log it.
+        /// </summary>
+        public MessageBoxResult ShowMessageBox(MessageBoxImage type, string message, MessageBoxButton buttons = MessageBoxButton.OK)
+        {
+            LogMessage(type, message);
+            return MessageBox.Show(message, string.Empty, buttons, type);
+        }
+
+        private void LogMessage(MessageBoxImage type, string message)
+        {
+            switch (type)
+            {
+                case MessageBoxImage.Error:
+                    _logger.Error(message);
+                    break;
+                case MessageBoxImage.Warning:
+                    _logger.Warn(message);
+                    break;
+            }
+        }
+    }
+}
