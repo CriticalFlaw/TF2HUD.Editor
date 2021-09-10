4c68dfb56c2691722f9cadb0cdd2da0a1652f1b2
Adam Swanson <adam.swanson.98@gmail.com>
2021-09-10 17:36:40 -0400

    clean: refactor HUD background application

--- src/TF2HUD.Editor/Classes/HUD/HUDBackground.cs
+++ src/TF2HUD.Editor/Classes/HUD/HUDBackground.cs
@@ -70,92 +70,109 @@ namespace HUDEditor.Classes
 
                 if (useCustomBackground && !string.IsNullOrWhiteSpace(customImagePath))
                 {
-                    // Check that the supplied image path is valid
-                    _logger.Info($"Validating image path: {customImagePath}");
-                    if (!Uri.TryCreate(customImagePath, UriKind.Absolute, out _)) return;
-
-                    // Move all existing files to the disabled folder.
-                    foreach (var filePath in Directory.GetFiles(consoleFolder))
-                    {
-                        _logger.Info($"Moving {filePath} to {disabledFolder}");
-                        File.Move(filePath, $"{disabledFolder}\\{filePath.Split("\\")[^1]}", true);
-                    }
-
-                    // Convert the provided image into a VTF format.
-                    var tf2Path = MainWindow.HudPath.Replace("\\tf\\custom\\", string.Empty);
-                    var output = $"{consoleFolder}\\background_upward.vtf";
-                    _logger.Info($"Converting image to VTF: {output}");
-                    _vtf.Convert(tf2Path, customImagePath, output);
-
-                    // Copy the generated file to the backgrounds folder.
-                    _logger.Info($"Copying {output} to {HUDFolderPath}");
-                    File.Copy(output, output.Replace("background_upward", "background_upward_widescreen"), true);
-                    File.Copy("Resources\\chapterbackgrounds.txt", $"{HUDFolderPath}\\scripts\\chapterbackgrounds.txt", true);
+                    ApplyCustomBackground(consoleFolder, disabledFolder);
+                }
+                else if (useHUDBackground)
+                {
+                    ApplyHUDBackground(consoleFolder, disabledFolder);
+                }
+                else if (useStockBackgrounds)
+                {
+                    DisableBackground(consoleFolder);
                 }
                 else
                 {
-                    if (useHUDBackground)
-                    {
-                        // Copy enabled background to console folder
-                        if (File.Exists($"{disabledFolder}\\{HUDImagePath}.vtf"))
-                        {
-                            _logger.Info($"Copying {disabledFolder}\\{HUDImagePath} to {consoleFolder}");
-                            File.Copy($"{disabledFolder}\\{HUDImagePath}.vtf", $"{consoleFolder}\\background_upward.vtf", true);
-                        }
-
-                        if (File.Exists($"{disabledFolder}\\{HUDImagePath}_widescreen.vtf"))
-                        {
-                            _logger.Info($"Copying {disabledFolder}\\{HUDImagePath} to {consoleFolder}");
-                            File.Copy($"{disabledFolder}\\{HUDImagePath}.vtf", $"{consoleFolder}\\background_upward_widescreen.vtf", true);
-                        }
-
-                        _logger.Info($"Copying chapterbackgrounds.txt to {HUDFolderPath}\\scripts");
-                        File.Copy("Resources\\chapterbackgrounds.txt", $"{HUDFolderPath}\\scripts\\chapterbackgrounds.txt", true);
-                    }
-                    else if (useStockBackgrounds)
-                    {
-                        foreach (var filePath in Directory.GetFiles(consoleFolder))
-                            if (filePath.EndsWith(".vtf"))
-                            {
-                                _logger.Info($"Disabling {filePath} (.VTF => .BAK)");
-                                File.Move(filePath, filePath.Replace(".vtf", ".bak"), true);
-                            }
-                            else if (filePath.EndsWith(".vmt"))
-                            {
-                                _logger.Info($"Disabling {filePath} (.VMT => .TEMP)");
-                                File.Move(filePath, filePath.Replace(".vmt", ".temp"), true);
-                            }
-                    }
-                    else
-                    {
-                        // Restore console folder
-                        if (!Directory.Exists(disabledFolder)) return;
-                        foreach (var filePath in Directory.GetFiles(disabledFolder))
-                        {
-                            _logger.Info($"Copying {filePath} to {consoleFolder}");
-                            File.Move(filePath, $"{consoleFolder}\\{filePath.Split("\\")[^1]}", true);
-                        }
-
-                        Directory.Delete(disabledFolder);
-
-                        foreach (var filePath in Directory.GetFiles(consoleFolder))
-                            if (filePath.EndsWith(".bak"))
-                            {
-                                _logger.Info($"Enabling {filePath} (.BAK => .VTF)");
-                                File.Move(filePath, filePath.Replace(".bak", ".vtf"), true);
-                            }
-                            else if (filePath.EndsWith(".temp"))
-                            {
-                                _logger.Info($"Enabling {filePath} (.TEMP => .VMT)");
-                                File.Move(filePath, filePath.Replace(".temp", ".vmt"), true);
-                            }
-                    }
+                    EnablePreviouslyDisabledBackground(consoleFolder, disabledFolder);
                 }
             }
             catch (Exception e)
             {
                 _notifier.ShowMessageBox(MessageBoxImage.Error, e.Message);
             }
+        }
+
+        private void EnablePreviouslyDisabledBackground(string consoleFolder, string disabledFolder)
+        {
+            // Restore console folder
+            if (!Directory.Exists(disabledFolder)) return;
+            foreach (var filePath in Directory.GetFiles(disabledFolder))
+            {
+                _logger.Info($"Copying {filePath} to {consoleFolder}");
+                File.Move(filePath, $"{consoleFolder}\\{filePath.Split("\\")[^1]}", true);
+            }
+
+            Directory.Delete(disabledFolder);
+
+            foreach (var filePath in Directory.GetFiles(consoleFolder))
+                if (filePath.EndsWith(".bak"))
+                {
+                    _logger.Info($"Enabling {filePath} (.BAK => .VTF)");
+                    File.Move(filePath, filePath.Replace(".bak", ".vtf"), true);
+                }
+                else if (filePath.EndsWith(".temp"))
+                {
+                    _logger.Info($"Enabling {filePath} (.TEMP => .VMT)");
+                    File.Move(filePath, filePath.Replace(".temp", ".vmt"), true);
+                }
+        }
+
+        private void DisableBackground(string consoleFolder)
+        {
+            foreach (var filePath in Directory.GetFiles(consoleFolder))
+                if (filePath.EndsWith(".vtf"))
+                {
+                    _logger.Info($"Disabling {filePath} (.VTF => .BAK)");
+                    File.Move(filePath, filePath.Replace(".vtf", ".bak"), true);
+                }
+                else if (filePath.EndsWith(".vmt"))
+                {
+                    _logger.Info($"Disabling {filePath} (.VMT => .TEMP)");
+                    File.Move(filePath, filePath.Replace(".vmt", ".temp"), true);
+                }
+        }
+
+        private void ApplyHUDBackground(string consoleFolder, string disabledFolder)
+        {
+            // Copy enabled background to console folder
+            if (File.Exists($"{disabledFolder}\\{HUDImagePath}.vtf"))
+            {
+                _logger.Info($"Copying {disabledFolder}\\{HUDImagePath} to {consoleFolder}");
+                File.Copy($"{disabledFolder}\\{HUDImagePath}.vtf", $"{consoleFolder}\\background_upward.vtf", true);
+            }
+
+            if (File.Exists($"{disabledFolder}\\{HUDImagePath}_widescreen.vtf"))
+            {
+                _logger.Info($"Copying {disabledFolder}\\{HUDImagePath} to {consoleFolder}");
+                File.Copy($"{disabledFolder}\\{HUDImagePath}.vtf", $"{consoleFolder}\\background_upward_widescreen.vtf", true);
+            }
+
+            _logger.Info($"Copying chapterbackgrounds.txt to {HUDFolderPath}\\scripts");
+            File.Copy("Resources\\chapterbackgrounds.txt", $"{HUDFolderPath}\\scripts\\chapterbackgrounds.txt", true);
+        }
+
+        private void ApplyCustomBackground(string consoleFolder, string disabledFolder)
+        {
+            // Check that the supplied image path is valid
+            _logger.Info($"Validating image path: {customImagePath}");
+            if (!Uri.TryCreate(customImagePath, UriKind.Absolute, out _)) return;
+
+            // Move all existing files to the disabled folder.
+            foreach (var filePath in Directory.GetFiles(consoleFolder))
+            {
+                _logger.Info($"Moving {filePath} to {disabledFolder}");
+                File.Move(filePath, $"{disabledFolder}\\{filePath.Split("\\")[^1]}", true);
+            }
+
+            // Convert the provided image into a VTF format.
+            var tf2Path = MainWindow.HudPath.Replace("\\tf\\custom\\", string.Empty);
+            var output = $"{consoleFolder}\\background_upward.vtf";
+            _logger.Info($"Converting image to VTF: {output}");
+            _vtf.Convert(tf2Path, customImagePath, output);
+
+            // Copy the generated file to the backgrounds folder.
+            _logger.Info($"Copying {output} to {HUDFolderPath}");
+            File.Copy(output, output.Replace("background_upward", "background_upward_widescreen"), true);
+            File.Copy("Resources\\chapterbackgrounds.txt", $"{HUDFolderPath}\\scripts\\chapterbackgrounds.txt", true);
         }
     }
 }
