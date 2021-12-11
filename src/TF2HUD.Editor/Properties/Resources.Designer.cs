31cbe59aa3133421fef21f450bfc87effe8e8053
CriticalFlaw <inikitin1@outlook.com>
2021-12-11 12:59:12 -0500

    Update kbnhud.json

--- src/TF2HUD.Editor/Properties/Resources.Designer.cs
+++ src/TF2HUD.Editor/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HUDEditor.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -313,7 +313,7 @@ namespace HUDEditor.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Report an Issue.
+        ///   Looks up a localized string similar to Report an Issue..
         /// </summary>
         public static string tooltip_report {
             get {
@@ -462,15 +462,6 @@ namespace HUDEditor.Properties {
         public static string ui_switch {
             get {
                 return ResourceManager.GetString("ui_switch", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to TF2 HUD Editor.
-        /// </summary>
-        public static string ui_title {
-            get {
-                return ResourceManager.GetString("ui_title", resourceCulture);
             }
         }
         
