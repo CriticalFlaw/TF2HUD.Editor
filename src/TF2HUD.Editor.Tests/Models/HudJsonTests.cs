36eb26c4f68a1a029923b2aa5e58e1173d2664b9
cooolbros <pfwobcke@gmail.com>
2024-01-01 12:16:49 +1100

    Add discord invite test

---
+++ src/TF2HUD.Editor.Tests/Models/HudJsonTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using HUDEditor.Models;
+
+namespace HUDEditorTests.Models.Tests;
+
+[TestClass()]
+public class HudJsonTests
+{
+    [TestMethod()]
+    public async Task HudJsonDiscordTest()
+    {
+        var jsonFiles = Directory.EnumerateFiles("..\\..\\..\\..\\TF2HUD.Editor\\JSON")
+            .Select((jsonFile) => JsonConvert.DeserializeObject<HudJson>(File.ReadAllText(jsonFile)))
+            .OfType<HudJson>()
+            .Where((hudJson) => hudJson.Links.Discord != null)
+            .ToArray();
+
+        var statusCodes = await Task.WhenAll(
+            jsonFiles.Select(async (hudJson) =>
+            {
+                var invite = new Uri(hudJson.Links.Discord).LocalPath.Split("/").Last();
+                var response = await new HttpClient().GetAsync($"https://discord.com/api/v10/invites/{invite}");
+                return response.StatusCode;
+            })
+        );
+
+        CollectionAssert.AreEqual(
+            Enumerable.Repeat(HttpStatusCode.OK, jsonFiles.Length).ToArray(),
+            statusCodes
+        );
+    }
+}
