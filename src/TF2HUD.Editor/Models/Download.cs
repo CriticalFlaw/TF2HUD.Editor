26d30b899d432ed285144b4babc5aab527652f28
Igor <nik_101@live.co.uk>
2024-09-01 13:21:47 -0400

    Separated model classes

---
+++ src/TF2HUD.Editor/Models/Download.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Windows;
+using Newtonsoft.Json.Linq;
+
+namespace HUDEditor.Models
+{
+    public class Download
+    {
+        public string Content => Source;
+        public string Source { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ src/TF2HUD.Editor/Models/Option.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Windows;
+using Newtonsoft.Json.Linq;
+
+namespace HUDEditor.Models
+{
+    public class Option
+    {
+        [JsonPropertyName("FileName")] public string FileName;
+        [JsonPropertyName("Files")] public JObject Files;
+        [JsonPropertyName("Label")] public string Label;
+        [JsonPropertyName("RenameFile")] public RenameFile RenameFile;
+        [JsonPropertyName("Special")] public string Special;
+        [JsonPropertyName("SpecialParameters")] public string[] SpecialParameters;
+        [JsonPropertyName("Value")] public string Value;
+    }
+}
