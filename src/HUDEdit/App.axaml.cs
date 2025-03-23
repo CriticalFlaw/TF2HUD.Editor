893054f3fc7406abd9176efc43e4a7a9905f08db
Igor <nik_101@live.co.uk>
2025-03-23 12:15:03 -0400

    Added Sentry

--- src/HUDEdit/App.axaml.cs
+++ src/HUDEdit/App.axaml.cs
@@ -1,11 +1,16 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Microsoft.Extensions.Configuration;
+using Sentry;
 
 namespace HUDEdit;
 
 public partial class App : Application
 {
+    public static IConfiguration Config { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -19,5 +24,30 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    public App()
+    {
+        // Load configuration from appconfig.json
+        Config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        Dispatcher.UIThread.UnhandledException += App_DispatcherUnhandledException;
+        SentrySdk.Init(o =>
+        {
+            // Tells which project in Sentry to send events to:
+            o.Dsn = Config["Sentry:Dsn"];
+            // When configuring for the first time, to see what the SDK is doing:
+            o.Debug = true;
+        });
+    }
+
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        SentrySdk.CaptureException(e.Exception);
+
+        // If you want to avoid the application from crashing:
+        e.Handled = true;
     }
 }
--- src/HUDEdit/MainWindow.axaml.cs
+++ src/HUDEdit/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Sentry;
 
 namespace HUDEdit;
 
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SentrySdk.CaptureMessage("Hello Sentry");
     }
 }
