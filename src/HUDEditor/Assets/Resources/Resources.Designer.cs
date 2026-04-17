1c2b676b6fadfbf192b55b208c94ee8e41895f19
Wenyin Root <ruojiner@hotmail.com>
2026-04-18 01:55:36 +0800

    fix: correct English strings in Resources.resx and add missing translations to all locale files

--- src/HUDEditor/Assets/Resources/Resources.Designer.cs
+++ src/HUDEditor/Assets/Resources/Resources.Designer.cs
@@ -61,105 +61,6 @@ namespace HUDEditor.Assets {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to and.
-        /// </summary>
-        public static string appinfo_and {
-            get {
-                return ResourceManager.GetString("appinfo_and", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to your favorite Team Fortress 2 HUDs!.
-        /// </summary>
-        public static string appinfo_favorite_huds {
-            get {
-                return ResourceManager.GetString("appinfo_favorite_huds", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Install.
-        /// </summary>
-        public static string appinfo_install {
-            get {
-                return ResourceManager.GetString("appinfo_install", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Version.
-        /// </summary>
-        public static string appinfo_version {
-            get {
-                return ResourceManager.GetString("appinfo_version", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to General backend fixes and improvements..
-        /// </summary>
-        public static string changelog_backend {
-            get {
-                return ResourceManager.GetString("changelog_backend", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Added missing crosshair options..
-        /// </summary>
-        public static string changelog_crosshair {
-            get {
-                return ResourceManager.GetString("changelog_crosshair", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Fixed scrollbars not being visible when available..
-        /// </summary>
-        public static string changelog_scrollbars {
-            get {
-                return ResourceManager.GetString("changelog_scrollbars", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Fixed tooltips not displaying..
-        /// </summary>
-        public static string changelog_tooltips {
-            get {
-                return ResourceManager.GetString("changelog_tooltips", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Added a label that shows the current user path in the settings menu..
-        /// </summary>
-        public static string changelog_user_path {
-            get {
-                return ResourceManager.GetString("changelog_user_path", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Implemented vtex2 for generating custom backgrounds on Linux.
-        /// </summary>
-        public static string changelog_vtex2 {
-            get {
-                return ResourceManager.GetString("changelog_vtex2", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Select an image file.
-        /// </summary>
-        public static string dialog_select_image {
-            get {
-                return ResourceManager.GetString("dialog_select_image", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to An error occurred while installing {0}..
         /// </summary>
         public static string error_hud_install {
@@ -174,24 +75,6 @@ namespace HUDEditor.Assets {
         public static string error_hud_uninstall {
             get {
                 return ResourceManager.GetString("error_hud_uninstall", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to No logs folder found..
-        /// </summary>
-        public static string error_logs_folder {
-            get {
-                return ResourceManager.GetString("error_logs_folder", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to No log files found..
-        /// </summary>
-        public static string error_no_log_files {
-            get {
-                return ResourceManager.GetString("error_no_log_files", resourceCulture);
             }
         }
         
@@ -385,33 +268,6 @@ namespace HUDEditor.Assets {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Settings.
-        /// </summary>
-        public static string settings_settings {
-            get {
-                return ResourceManager.GetString("settings_settings", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Troubleshooting.
-        /// </summary>
-        public static string settings_troubleshooting {
-            get {
-                return ResourceManager.GetString("settings_troubleshooting", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to User Path.
-        /// </summary>
-        public static string settings_user_path {
-            get {
-                return ResourceManager.GetString("settings_user_path", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to {0} customizations applied on {1}..
         /// </summary>
         public static string status_applied {
@@ -543,15 +399,6 @@ namespace HUDEditor.Assets {
         public static string ui_browse {
             get {
                 return ResourceManager.GetString("ui_browse", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Cancel.
-        /// </summary>
-        public static string ui_cancel {
-            get {
-                return ResourceManager.GetString("ui_cancel", resourceCulture);
             }
         }
         
--- src/HUDEditor/ViewModels/AppInfoViewModel.cs
+++ src/HUDEditor/ViewModels/AppInfoViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace HUDEditor.ViewModels;
+using System.Reflection;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HUDEditor.ViewModels;
 
 internal partial class AppInfoViewModel : ViewModelBase
 {
-
+    public string AppVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString(2) ?? "4.4";
 }
